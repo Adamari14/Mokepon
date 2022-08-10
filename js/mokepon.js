7bfe55566e43f7851212abc66ef3d5bca9c79f6a
Adamari14 <adamarim_15@hotmail.com>
2022-08-10 15:45:44 -0500

    llamando argumentos, usando eventos en JavaScript

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador() {
+    alert("Seleccionaste tu mascota")
+}
+
+let botonMascotaJuador = document.getElementById('boton-mascota') //llamar el argumento en html
+botonMascotaJuador.addEventListener('click' , seleccionarMascotaJugador) //evento , funcion
