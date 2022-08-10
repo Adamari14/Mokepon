f9aa385b1839d755a7563086e7d23129a3aa4ea9
Adamari14 <adamarim_15@hotmail.com>
2022-08-10 16:59:10 -0500

    logica de inputs con condicionales

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,26 @@
-function seleccionarMascotaJugador() {
-    alert("Seleccionaste tu mascota")
+function iniciarJuego(){ 
+    let botonMascotaJuador = document.getElementById('boton-mascota') //llamar el argumento en html
+    botonMascotaJuador.addEventListener('click' , seleccionarMascotaJugador) //evento , funcion
 }
 
-let botonMascotaJuador = document.getElementById('boton-mascota') //llamar el argumento en html
-botonMascotaJuador.addEventListener('click' , seleccionarMascotaJugador) //evento , funcion
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if(inputHipodoge.checked) {
+        alert("Seleccionaste a Hipodoge")
+    } else if(inputCapipepo.checked) {
+        alert("Seleccionaste a Capipepo")
+    } else if(inputRatigueya.checked) {
+        alert("Seleccionaste a Ratigueya")
+    } else {
+        alert("Selecciona una mascota")
+    }
+       
+
+
+   
+}
+
+window.addEventListener('load',iniciarJuego)
