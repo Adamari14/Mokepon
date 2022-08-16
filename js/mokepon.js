6ce1a47ee322948fe6f2fdb63ed05b6c695fc5f9
Adamari14 <adamarim_15@hotmail.com>
2022-08-16 16:37:04 -0500

    Ataques dinámicos por cada mascota: extraer

--- js/mokepon.js
+++ js/mokepon.js
@@ -28,6 +28,7 @@ let opcionDeMokepones
 let inputHipodoge
 let inputCapipepo
 let inputRatigueya
+let mascotaJugador
 let vidasJugador = 3
 let vidasEnemigo = 3
 
@@ -109,15 +110,29 @@ function seleccionarMascotaJugador() {
 
     if(inputHipodoge.checked) { //Manipulando DOM nombre de mascota-jugador
         spanMascotaJugador.innerHTML = inputHipodoge.id
+        mascotaJugador = inputHipodoge.id
     } else if(inputCapipepo.checked) {
         spanMascotaJugador.innerHTML = inputCapipepo.id
+        mascotaJugador = inputCapipepo.id
     } else if(inputRatigueya.checked) {
         spanMascotaJugador.innerHTML = inputRatigueya.id
+        mascotaJugador = inputRatigueya.id
     } else {
         alert("Selecciona una mascota")
     }   
 
+    extraerAtaques(mascotaJugador)
     seleccionarMascotaEnemigo()
+}
+
+function extraerAtaques(mascotaJugador){
+    let ataques 
+    for (let i = 0; i < mokepones.length; i++) {
+        if(mascotaJugador === mokepones[i].nombre){
+            ataques = mokepones[i].ataques
+        }
+    }
+    console.log(ataques)
 }
 
 function seleccionarMascotaEnemigo(){
